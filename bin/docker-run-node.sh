7625089c21ee087ff63f7382d616418f4301160b
mcpersiko <mcpersiko@gmail.com>
2024-10-08 21:05:24 -0400

    Prebackstage Dockerfile builds base image

---
+++ bin/docker-run-node.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -P -v /Users/mcp/projects/devsmcp/app:/app --name barn pre-backstage-node bash
---
+++ bin/docker-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -P -v /Users/mcp/projects/devsmcp/app:/app --name bark prebackstage bash
